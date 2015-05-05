6443551145e6b106d87738f7eb113afae7b56f0e
Aaron Washburn <aaronwashburn@Aarons-MacBook-Pro.local>
2015-05-05 10:14:56 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bleval::Application.config.secret_key_base = '4760f3b757415910cc39a66d80a5357c20983013ef956293408ca02b0dd6ed85bd1a09b5cce19b26f2f02209509c101adb1f4981bd6a0711afa3c7cd42020753'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Bleval::Application.config.session_store :cookie_store, key: '_bleval_session'
